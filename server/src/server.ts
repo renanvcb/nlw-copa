0244a85bf6810a3f2807b4d3ac64a49fa80b0a83
Renan Borges <renanvcb@gmail.com>
2022-11-02 14:04:05 -0300

    new routes added

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,7 @@
 import Fastify from 'fastify'
 import cors from '@fastify/cors'
+import { z } from 'zod'
+import ShortUniqueId from 'short-unique-id'
 import { PrismaClient } from '@prisma/client'
 
 const prisma  = new PrismaClient({
@@ -17,6 +19,38 @@ async function bootstrap() {
 
   fastify.get('/pools/count', async () => {
     const count = await prisma.pool.count()
+    
+    return {count}
+  })
+
+  fastify.post('/pools', async (request, reply) => {
+    const createPoolBody = z.object({
+      title: z.string(),
+    })
+
+    const { title } = createPoolBody.parse(request.body)
+
+    const generate = new ShortUniqueId({length: 6})
+    const poolCode = String(generate()).toUpperCase()
+
+    await prisma.pool.create({
+      data: {
+        title,
+        code: poolCode
+      }
+    })
+
+    return reply.status(201).send({ poolCode })
+  })
+
+  fastify.get('/user/count', async () => {
+    const count = await prisma.user.count()
+    
+    return {count}
+  })
+
+  fastify.get('/guess/count', async () => {
+    const count = await prisma.guess.count()
     
     return {count}
   })
